63190f181f434bab0fba41a6a068526a5e8eb948
Michał Wadas <you@example.com>
2019-10-20 18:37:37 +0200

    zadanie5 i 6

---
+++ zadanie5.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+int  main (){
+ 	 cout << "podaj liczbe" endl;  
+	int i;
+	cin >> i ;
+	cout << &i;
+	return 0;
+}
---
+++ zadanie6.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+
+	 int *i;
+	 int j = 123;
+	 i = &j;
+	 cout << *i;
+  	 
+	 return 0;
+
+
+}
