e2df521673602e7c929fbe47b7b0fe29c2c374cb
Michal Wadas <s20495@pjwstk.edu.pl>
2019-11-03 15:58:03 +0100

    Nowy Folder

---
+++ zjazd2/zadanie4.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+//Badysiak Paweł
+//Wadas Michał
+int main(){
+ 	cout << "podaj lczby" << endl;
+	int i;
+	int j=0;
+	cin >> i;
+	while( i !=0 ){
+		j = j +i;
+		cin >> i;
+}
+	cout << "twoja suma to: " << j ;
+
+}
